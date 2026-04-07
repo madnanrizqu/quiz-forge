f65ba84fca5eab358a44a5c8baffec9c1601e38b
Muhammad Adnan Rizqullah <madnanrizqullah@gmail.com>
2026-04-08 06:28:09 +0700

    chore: add state management tanstack query and storybook

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -1,9 +1,14 @@
+// For more info, see https://github.com/storybookjs/eslint-plugin-storybook#configuration-flat-config-format
+import storybook from 'eslint-plugin-storybook'
+
 //  @ts-check
 
 import { tanstackConfig } from '@tanstack/eslint-config'
+import pluginQuery from '@tanstack/eslint-plugin-query'
 
 export default [
   ...tanstackConfig,
+  ...pluginQuery.configs['flat/recommended-strict'],
   {
     rules: {
       'import/no-cycle': 'off',
@@ -17,4 +22,5 @@ export default [
   {
     ignores: ['eslint.config.js', 'prettier.config.js'],
   },
+  ...storybook.configs['flat/recommended'],
 ]
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,20 +1,59 @@
+/// <reference types="vitest/config" />
 import { defineConfig } from 'vite'
 import { devtools } from '@tanstack/devtools-vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
-
 import { tanstackRouter } from '@tanstack/router-plugin/vite'
-
 import viteReact from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+import path from 'node:path'
+import { fileURLToPath } from 'node:url'
+import { storybookTest } from '@storybook/addon-vitest/vitest-plugin'
 
+const dirname =
+  typeof __dirname !== 'undefined'
+    ? __dirname
+    : path.dirname(fileURLToPath(import.meta.url))
+
+// More info at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon
 const config = defineConfig({
   plugins: [
     devtools(),
-    tsconfigPaths({ projects: ['./tsconfig.json'] }),
+    tsconfigPaths({
+      projects: ['./tsconfig.json'],
+    }),
     tailwindcss(),
-    tanstackRouter({ target: 'react', autoCodeSplitting: true }),
+    tanstackRouter({
+      target: 'react',
+      autoCodeSplitting: true,
+    }),
     viteReact(),
   ],
+  test: {
+    workspace: [
+      {
+        extends: true,
+        plugins: [
+          // The plugin will run tests for the stories defined in your Storybook config
+          // See options at: https://storybook.js.org/docs/next/writing-tests/integrations/vitest-addon#storybooktest
+          storybookTest({
+            configDir: path.join(dirname, '.storybook'),
+          }),
+        ],
+        test: {
+          name: 'storybook',
+          browser: {
+            enabled: true,
+            headless: true,
+            provider: 'playwright',
+            instances: [
+              {
+                browser: 'chromium',
+              },
+            ],
+          },
+        },
+      },
+    ],
+  },
 })
-
 export default config
